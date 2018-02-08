486776d4bcc313d15d8c5a4d254f787f44107631
alex <alex.xie.gm@gmail.com>
2018-02-08 23:46:34 +0800

    upadted

--- auto.py
+++ auto.py
@@ -96,7 +96,7 @@ for document in db.Filter.find():
     ]
 
     result = db.Game.aggregate(query)
-    print(json.dumps(list(result), indent=4, ensure_ascii=False))
+    print(json.dumps(list(result), indent=4, encoding='utf-8', ensure_ascii=False))
     # db.Result.insert_many(list(result))
 
 
