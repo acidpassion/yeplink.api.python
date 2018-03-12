5a7bbdda1e55ebdf85e96b2b00dbef322e5c243f
alex <alex.xie.gm@gmail.com>
2018-03-12 22:49:28 +0800

    updated

--- auto_option2.py
+++ auto_option2.py
@@ -14,40 +14,49 @@ for document in db.Filter.find():
 
     query = [{
         "$lookup": {
-            "from": "Game",
+            "from": "GameDetail",
             "localField": "gameId",
             "foreignField": "gameId",
-            "as": "game"
+            "as": "details"
         }
     },{
         "$match": {
             "$and": [{
 
-                "game.date": today,
-                "startHost": {
+                "date": today,
+                "details.startHost": {
                     "$gte": filter["StartHostFrom"],
                     "$lte": filter["StartHostTo"]
                 },
-                "startPanko": filter["StartPanko"],
-                "startGuest": {
+                "details.startPanko": filter["StartPanko"],
+                "details.startGuest": {
                     "$gte": filter["StartGuestFrom"],
                     "$lte": filter["StartGuestTo"]
                 },
-                "nowHost": {
+                "details.endHost": {
+                    "$gte": filter["EndHostFrom"],
+                    "$lte": filter["EndHostTo"]
+                },
+                "details.endPanko": filter["EndPanko"],
+                "details.endGuest": {
+                    "$gte": filter["EndGuestFrom"],
+                    "$lte": filter["EndGuestTo"]
+                },
+                "details.nowHost": {
                     "$gte": filter["NowHostFrom"],
                     "$lte": filter["NowHostTo"]
                 },
-                "nowPanko": filter["NowPanko"],
-                "nowGuest": {
+                "details.nowPanko": filter["NowPanko"],
+                "details.nowGuest": {
                     "$gte": filter["NowGuestFrom"],
                     "$lte": filter["NowGuestTo"]
                 },
-                "euroAsiaHost": {
+                "details.euroAsiaHost": {
                     "$gte": filter["EuroAsiaHostFrom"],
                     "$lte": filter["EuroAsiaHostTo"]
                 },
-                "euroAsiaPanko": filter["EuroAsiaPanko"],
-                "euroAsiaGuest": {
+                "details.euroAsiaPanko": filter["EuroAsiaPanko"],
+                "details.euroAsiaGuest": {
                     "$gte": filter["EuroAsiaGuestFrom"],
                     "$lte": filter["EuroAsiaGuestTo"]
                 }
@@ -56,24 +65,27 @@ for document in db.Filter.find():
         }
     },
         {
-            "$unwind": "$game"
+            "$unwind": "$details"
         },
         {
         "$project": {
             "fullname": {
-                "$concat": ["$game.type", " ", "$game.date", " ", "$game.time", " ", "$game.host", " VS ", "$game.guest"]
+                "$concat": ["$type", " ", "$date", " ", "$time", " ", "$host", " VS ", "$guest"]
             },
             "_id": 0,
-            "company": "$company",
-            "startHost": "$startHost",
-            "startPanko": "$startPanko",
-            "startGuest": "$startGuest",
-            "nowHost": "$nowHost",
-            "nowPanko": "$nowPanko",
-            "nowGuest": "$nowGuest",
-            "euroAsiaHost": "$euroAsiaHost",
-            "euroAsiaPanko": "$euroAsiaPanko",
-            "euroAsiaGuest": "$euroAsiaGuest"
+            "company": "$details.company",
+            "startHost": "$details.startHost",
+            "startPanko": "$details.startPanko",
+            "startGuest": "$details.startGuest",
+            "nowHost": "$details.nowHost",
+            "nowPanko": "$details.nowPanko",
+            "nowGuest": "$details.nowGuest",
+            "endHost": "$details.endHost",
+            "endPanko": "$details.endPanko",
+            "endGuest": "$details.endGuest",
+            "euroAsiaHost": "$details.euroAsiaHost",
+            "euroAsiaPanko": "$details.euroAsiaPanko",
+            "euroAsiaGuest": "$details.euroAsiaGuest"
         }
     },
         {
@@ -86,7 +98,7 @@ for document in db.Filter.find():
         }
     ]
 
-    result = db.GameDetail.aggregate(query)
+    result = db.Game.aggregate(query)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     db.Result.delete_many({"filterID": filter['_id'], "date": today})
     data = []
